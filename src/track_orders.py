c7eec84d3ae8a476c48893dd981d9b86c5ad85e0
Matheus Martino <matheus.martino97@gmail.com>
2021-12-02 23:42:43 -0300

    Req 2.

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,49 @@
+from src.analyze_log import (
+    get_days_off_by_client,
+    get_most_ordered_by_client,
+    get_never_ordered_by_client
+)
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+        self.days = {}
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
-    def add_new_order(self, costumer, order, day):
-        pass
+    def add_new_order(self, customer, order, day):
+        self.orders.append((customer, order, day))
 
-    def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        if day not in self.days:
+            self.days[day] = 1
+        else:
+            self.days[day] += 1
 
-    def get_never_ordered_per_costumer(self, costumer):
-        pass
+    def get_most_ordered_dish_per_costumer(self, customer):
+        return get_most_ordered_by_client(customer, self.orders)
 
-    def get_days_never_visited_per_costumer(self, costumer):
-        pass
+    def get_never_ordered_per_costumer(self, customer):
+        return get_never_ordered_by_client(customer, self.orders)
+
+    def get_days_never_visited_per_costumer(self, customer):
+        return get_days_off_by_client(customer, self.orders)
 
     def get_busiest_day(self):
-        pass
+        day_index = 0
+        quantity_index = 1
+
+        def extractor(day_quantity):
+            return day_quantity[quantity_index]
+
+        return max(self.days.items(), key=extractor)[day_index]
 
     def get_least_busy_day(self):
-        pass
+        day_index = 0
+        quantity_index = 1
+
+        def extractor(day_quantity):
+            return day_quantity[quantity_index]
+
+        return min(self.days.items(), key=extractor)[day_index]
